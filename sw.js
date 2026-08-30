a5d3fde4435b8eddb6c5f52b76e74a7a5a561374
bowki70-ops <bowki70@gmail.com>
2026-08-30 11:45:50 +0900

    Provide PNG icon assets for Chrome PWA validation compliance

--- sw.js
+++ sw.js
@@ -2,7 +2,9 @@ const CACHE_NAME = 'yongin-parkgolf-v1';
 const ASSETS_TO_CACHE = [
   '/',
   '/index.html',
-  '/manifest.json'
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png'
 ];
 
 self.addEventListener('install', (event) => {
